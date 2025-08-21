9880b75ec9a205377da8fda18d0fdfce29aae672
espeestudiante265 <espeestudiante265@gmail.com>
2025-08-21 16:15:17 -0500

    Actualizacion

--- proyecto DSS1/backend/manage.py
+++ proyecto DSS1/backend/manage.py
@@ -1,11 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-# habilita PyMySQL como reemplazo de MySQLdb
+# Habilita PyMySQL como reemplazo de MySQLdb (debe ir antes de cualquier import de Django)
 import pymysql
 pymysql.install_as_MySQLdb()
 
-
-
-#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -13,7 +12,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    # Si DJANGO_SETTINGS_MODULE no viene del entorno, usa backend.settings por defecto
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'backend.settings'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
